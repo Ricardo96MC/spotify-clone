2e86d8f4399de5bcbdce90ea38c8ff7fc9a7422d
Ricardo-MAC <ismaelcod4@gmail.com>
2024-02-07 20:11:09 -0600

    Adding providers for supabase and user authentication

--- providers/SupabaseProvider.tsx
+++ providers/SupabaseProvider.tsx
@@ -1,25 +1,24 @@
-"use client"
+'use client'
 
-import { Database } from "@/types_db"
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { SessionContextProvider } from "@supabase/auth-helpers-react";
-import { useState } from "react";
+import { type Database } from '@/types_db'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
 
 interface SupabaseProviderProps {
-    children: React.ReactNode;
+    children: React.ReactNode
 };
 
 const SupabaseProvider = ({ children }: SupabaseProviderProps) => {
-
     const [supabaseClient] = useState(() =>
         createClientComponentClient<Database>()
-    );
+    )
 
     return (
         <SessionContextProvider supabaseClient={supabaseClient}>
             {children}
         </SessionContextProvider>
-    );
+    )
 }
 
-export default SupabaseProvider
+export default SupabaseProvider;
